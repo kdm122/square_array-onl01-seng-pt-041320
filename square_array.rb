e676d757175ff0fbcb47b600664f6db7614cdd6e
Kyle McLean <kyledmclean@gmail.com>
2020-04-23 02:48:04 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,3 +1,18 @@
+array = ["1","3","5"]
+
 def square_array(array)
   # your code here
+  
+  new_numbers = []
+  
+  array.each do |old_number|
+      new_number = old_number * old_number
+#    new_numbers.push = new_number
+      puts "gimme that #{old_number} which is now #{new_number}"
+  end
+  
+  
+    
 end
+
+square_array(array)
